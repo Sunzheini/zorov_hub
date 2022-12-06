df8d608b0b0933fc42b0fe7a2aec491613e741e3
Sunzheini <daniel_zorov@abv.bg>
2022-12-06 14:58:57 +0200

    update_06122022

--- zorov_hub/main_app/views.py
+++ zorov_hub/main_app/views.py
@@ -1,39 +1,60 @@
 import random
+from datetime import datetime
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+# ----------------------------------------------------------------------
+class ListDisplay:
+    @staticmethod
+    def display_list():
+        return ['item1', 'item22', 'item3']
+
+# ----------------------------------------------------------------------
 
 
 def index(request):
-    return HttpResponse('page: index')
+    return render(request, 'index.html')
 
 
 def games(request):
-    return HttpResponse('page: games')
+    return render(request, 'games.html')
 
 
 def shopping_list(request):
-    return HttpResponse('page: shopping list')
+    return render(request, 'shopping_list.html')
 
 
 # mikro-kontrolerite vkyshti naglasi
 def control_of_home(request):
-    return HttpResponse('page: control of home')
+    return render(request, 'control_of_home.html')
 
 
 # avotmatizaciq bylgarene  / solarni paneli
 def control_of_garden(request):
-    return HttpResponse('page: control of garden')
+    return render(request, 'control_of_garden.html')
 
 
 def chat(request):
-    return HttpResponse('page: chat')
+    return render(request, 'chat.html')
 
 
+# ----------------------------------------------------------------------
 def process_description(request, process_id):
     context = {
         'process_id': process_id,
         'my_info': [4, 5, 6],
         'value': random.random(),
         'a_dict': {'first_item': 11, 'second_item': '22'},
+        'from_class': ListDisplay.display_list(),  # hubavo e da se izpylbnqvat tuk
+        'datetime': datetime.now(),
     }
     return render(request, 'process_description.html', context)
+
+
+# ----------------------------------------------------------------------
+def return_to_home(request):
+    return redirect('index')
+
+
+
--- zorov_hub/settings.py
+++ zorov_hub/settings.py
@@ -1,3 +1,4 @@
+from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,7 +107,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    join(BASE_DIR, 'staticfiles'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
